a1f23dc481ed0d9ef8f670e1cc3ea21a177a8b8f
onesixtwo <vargaskenneth789@gmail.com>
2023-11-17 00:00:05 +0800

    commit

--- routes/api/temperature.js
+++ routes/api/temperature.js
@@ -18,7 +18,6 @@ router.post('/add', (req, res) => {
    })
 });
 
-
 //select / view
 //routes get api/temperature/view
 //@desc can view data
@@ -31,7 +30,37 @@ router.get('/view',(req,res) => {
    })
 })
 //update
+//routes PUT api/temperature/update/:id
+//@desc Update temperature data by ID
+//@access PRIVATE
+router.put('/update/:id', (req, res) => {
+   const { temperature, date, device_id } = req.body;
+   const temperatureId = req.params.id;
 
-//delete
+   if (!temperature || !date || !device_id) {
+       return res.status(400).json({ msg: 'Please provide temperature, date, and device_id' });
+   }
+
+   const sqlQuery = `UPDATE temp SET temperature = ${temperature}, date = '${date}', device_id = '${device_id}' WHERE id = ${temperatureId}`;
+
+   databaseConn.query(sqlQuery, function (error, results, fields) {
+       if (error) throw error;
+       res.status(200).json(results);
+   });
+});
+
+// delete
+// route: DELETE api/temperature/delete/:id
+// @desc: Delete data by ID
+// @access: PRIVATE
+router.delete('/delete/:id', (req, res) => {
+   const id = req.params.id;
+   sqlQuery = `DELETE FROM temp WHERE id = ${id}`;
+
+   databaseConn.query(sqlQuery, function (error, results, fields) {
+       if (error) throw error;
+       res.status(200).json(results);
+   });
+});
 
 module.exports = router;
